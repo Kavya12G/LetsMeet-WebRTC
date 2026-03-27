aaf793396514e2e45fbcf0372ae887d23607fb2b
Kavya12G <kavyagidaveer@gmail.com>
2026-03-27 11:44:30 +0530

    fix: remove EmailStr dependency

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 class UserCreate(BaseModel):
     username: str
-    email: EmailStr
+    email: str
     password: str
 
 class UserResponse(BaseModel):
     id: int
     username: str
-    email: EmailStr
+    email: str
 
     class Config:
         from_attributes = True
